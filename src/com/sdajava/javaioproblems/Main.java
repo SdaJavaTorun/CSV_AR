8ac899d75b11b647da034e98a0fa1ac0050ca45a
grovas <arczir@gmail.com>
2017-04-03 19:38:11 +0200

    group by year

--- src/com/sdajava/javaioproblems/Main.java
+++ src/com/sdajava/javaioproblems/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -23,20 +24,47 @@ public class Main {
                     .map(line -> {
                        String[] field = pattern.split(line);
                        return new Player(Integer.parseInt(field[0]),
-                               field[1],
-                               field[2],
-                               field[3],
-                               Integer.parseInt(field[4]));
+                                       field[1],
+                                       field[2],
+                                       field[3],
+                                       Integer.parseInt(field[4]));
                     })
                     .collect(Collectors.toList());
 
+           // Map<Integer, List<Player>> grouped = players.stream()
+             //       .collect(Collectors.groupingBy(x -> x.getYearID()));
+
+            Map<Integer, Long> groupedByYear = players
+                    .stream()
+                    .collect(Collectors.groupingBy(x -> x.getYearID(), Collectors.counting()));
+
             //players.stream().forEach(System.out::println);
             //for (Player p: players) System.out.println(p.yearID);
-            players.forEach(e -> System.out.println(e.yearID));
+            //players.forEach(e -> System.out.println(e.getYearID()));
+/*
+            Map<Integer, List<Player>> grouped = in
+                    .lines()
+                    .skip(1)
+                    .map(line -> {
+                        String[] field = pattern.split(line);
+                        return new Player(Integer.parseInt(field[0]),
+                                field[1],
+                                field[2],
+                                field[3],
+                                Integer.parseInt(field[4]));
+                    })
+                    .collect(Collectors.groupingBy(x -> x.getYearID()));
+*/
+            groupedByYear.entrySet().stream().forEach(System.out::println);
+
+            for (Player pl: players) {
+
+            }
 
         } catch (IOException err) {
             System.out.println("Error: " + err.getMessage());
         }
+
 
     }
 }
--- src/com/sdajava/javaioproblems/Player.java
+++ src/com/sdajava/javaioproblems/Player.java
@@ -4,7 +4,7 @@ package com.sdajava.javaioproblems;
 // taka klasa to obiekt POJO
 public class Player {
 
-    public int yearID;
+    private int yearID;
     private String teamID;
     private String lgID;
     private String playerID;
@@ -16,5 +16,56 @@ public class Player {
         this.lgID = lgID;
         this.playerID = playerID;
         this.salary = salary;
+    }
+
+    public int getYearID() {
+        return yearID;
+    }
+
+    public void setYearID(int yearID) {
+        this.yearID = yearID;
+    }
+
+    public String getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(String teamID) {
+        this.teamID = teamID;
+    }
+
+    public String getLgID() {
+        return lgID;
+    }
+
+    public void setLgID(String lgID) {
+        this.lgID = lgID;
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(String playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "yearID=" + yearID +
+                ", teamID='" + teamID + '\'' +
+                ", lgID='" + lgID + '\'' +
+                ", playerID='" + playerID + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
